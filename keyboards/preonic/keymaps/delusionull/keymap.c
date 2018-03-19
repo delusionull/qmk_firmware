1eea5c29021dc1a25713d1c2dbff7aeced9c7d3d
delusionull <delusionull@users.noreply.github.com>
2018-03-18 20:50:19 -0500

    Adds right mods; removes double function from space key

--- keyboards/preonic/keymaps/delusionull/keymap.c
+++ keyboards/preonic/keymaps/delusionull/keymap.c
@@ -52,19 +52,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Tab  |   "  |   ,  |   .  |   P  |   Y  |   F  |   G  |   C  |   R  |   L  | Del  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Esc  |   A  |   O  |   E  |   U  |   I  |   D  |   H  |   T  |   N  |   S  |  /   |
+ * | Esc  |   A  |   O  |   E  |   U  |   I  |   D  |   H  |   T  |   N  |   S  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Shift|   ;  |   Q  |   J  |   K  |   X  |   B  |   M  |   W  |   V  |   Z  |Enter |
+ * | Shift|   ;  |   Q  |   J  |   K  |   X  |   B  |   M  |   W  |   V  |   Z  |Sft \ |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Brite| Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
+ * | Hyper| Meh  | Alt  | GUI  |Lower |    Space    |Raise | GUI  | Alt  | Meh  | Hyper|
  * `-----------------------------------------------------------------------------------'
  */
 [_DVORAK] = {
-  {KC_GRV,        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,           KC_6,           KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS},
-  {KC_TAB,        KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,           KC_F,           KC_G,    KC_C,    KC_R,    KC_L,    KC_BSPC},
-  {CTL_T(KC_ESC), KC_A,    KC_O,    KC_E,    KC_U,    KC_I,           KC_D,           KC_H,    KC_T,    KC_N,    KC_S,    RCTL_T(KC_ENT)},
-  {TD(TD_SH_CPS), KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,           KC_B,           KC_M,    KC_W,    KC_V,    KC_Z,    RSFT_T(KC_SLSH)},
-  {BACKLIT,       _______, KC_LALT, KC_LGUI, LOWER,   RCTL_T(KC_SPC), RCTL_T(KC_SPC), RAISE,   _______, _______, _______, _______}
+  {KC_GRV,        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS},
+  {KC_TAB,        KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_BSPC},
+  {CTL_T(KC_ESC), KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    RCTL_T(KC_ENT)},
+  {TD(TD_SH_CPS), KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    RSFT_T(KC_SLSH)},
+  {KC_HYPR,       KC_MEH,  KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_RGUI, KC_RALT, KC_MEH,  KC_HYPR}
 },
 
 /* Qwerty
@@ -77,15 +77,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Shft "|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Brite| Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
+ * | Hyper| Meh  | Alt  | GUI  |Lower |    Space    |Raise | GUI  | Alt  | Meh  | Hyper|
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = {
-  {KC_GRV,        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,           KC_6,           KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS},
-  {KC_TAB,        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,           KC_Y,           KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
-  {CTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,           KC_H,           KC_J,    KC_K,    KC_L,    KC_SCLN, RCTL_T(KC_ENT)},
-  {TD(TD_SH_CPS), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,           KC_N,           KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_QUOT)},
-  {BACKLIT,       _______, KC_LALT, KC_LGUI, LOWER,   RCTL_T(KC_SPC), RCTL_T(KC_SPC), RAISE,   _______, _______, _______, _______}
+  {KC_GRV,        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS},
+  {KC_TAB,        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
+  {CTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, RCTL_T(KC_ENT)},
+  {TD(TD_SH_CPS), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_QUOT)},
+  {KC_HYPR,       KC_MEH,  KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_RGUI, KC_RALT, KC_MEH,  KC_HYPR}
 },
 
 /* Lower
